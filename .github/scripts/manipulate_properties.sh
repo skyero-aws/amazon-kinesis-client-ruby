7b997f23efd5d4c2b44bf9c8b387d12d1cad97c0
skye rogers <skyero@amazon.com>
2025-06-30 09:45:17 -0700

    change stream arn

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -6,16 +6,19 @@ set -e
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   sed -i "" "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
   sed -i "" "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
+  sed -i "" 's/us-east-5/us-east-1/g' samples/sample.properties
   grep -v "idleTimeBetweenReadsInMillis" samples/sample.properties > samples/temp.properties
   echo "idleTimeBetweenReadsInMillis = 250" >> samples/temp.properties
   mv samples/temp.properties samples/sample.properties
 elif [[ "$RUNNER_OS" == "Linux" ]]; then
   sed -i "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
   sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
+  sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
 elif [[ "$RUNNER_OS" == "Windows" ]]; then
   sed -i "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
   sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
+  sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
 else
   echo "Unknown OS: $RUNNER_OS"
