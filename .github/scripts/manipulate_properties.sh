49131ca1b1df2e5679367b7149c01baaac5895ee
skye rogers <skyero@amazon.com>
2025-06-30 11:27:08 -0700

    fix stream + app name and fix windows execution

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -20,6 +20,10 @@ elif [[ "$RUNNER_OS" == "Windows" ]]; then
   sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
   sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
+
+  echo '@echo off' > samples/run_script.bat
+  echo 'ruby %~dp0\sample_kcl.rb %*' >> samples/run_script.bat
+  sed -i 's/executableName = sample_kcl.rb/executableName = samples\/run_script.bat/' samples/sample.properties
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
